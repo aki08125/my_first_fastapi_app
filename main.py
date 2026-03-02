daeedf9dd6b48a77b787ac49e21049634acf85b1
aki08125 <urarara329@gmail.com>
2026-03-02 13:46:20 +0900

    step1セッション

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# 1. FastAPIの本体（アプリ）を作る
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Griff_SQLFAstudy21@localhost:3306/my_first_fastapi_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"  
+    
+    id = Column(Integer, primary_key=True, index=True) 
+    username = Column(String(50)) 
+
+SessionLocal = sessionmaker()
 app = FastAPI()
 
-# 2. HTMLファイルを置く場所（templatesフォルダ）を登録する
 templates = Jinja2Templates(directory="templates")
 
-# --- ここから「窓口（ルート）」の作成 ---
-
-# 【GET窓口】ブラウザで「/」にアクセスしたときに、入力画面を表示する
 @app.get("/", response_class=HTMLResponse)
 async def read_form(request: Request):
-    # index.htmlを表示してください、という命令
     return templates.TemplateResponse("index.html", {"request": request})
 
-# 【POST窓口】「送信」ボタンが押されたときに、名前を受け取って返事をする
 @app.post("/register")
 async def register(username: str = Form(...)):
-    # 届いた名前（username）を使ってメッセージを作る
     return {"message": f"こんにちは、{username}さん！FastAPIへようこそ！"}
+
+
+
+
+"""
+こうすれば、PythonがHTMLを書き換えれる
+
+@app.post("/register", response_class=HTMLResponse)
+async def register(request: Request, username: str = Form(...)): 
+    return templates.TemplateResponse(
+        "index.html", 
+        {
+            "request": request,    
+            "user_name": username  
+        }
+    )
+"""
